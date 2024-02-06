cb3a00abe9ed03615ab604de54b04dd25c3cef8a
Mathilda <mathilda.magnusson@gmail.com>
2024-02-06 11:20:24 +0100

    init

---
+++ myfirstreactapp/src/Hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Hello() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <p>pop</p>
+      <p>fejefjej</p>
+    </div>
+  );
+}
+export default Hello;
