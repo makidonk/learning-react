8a2fcabda526e503c0887f928eb8fb60b21a787a
Mathilda <mathilda.magnusson@gmail.com>
2024-03-04 15:25:51 +0100

    starting

---
+++ myfirstreactapp/src/ListItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ListItem(value) {
+  return (
+    <li>
+      <label>{value.name}</label>
+    </li>
+  );
+}
+
+export default ListItem;
--- myfirstreactapp/src/index.js
+++ myfirstreactapp/src/index.js
@@ -1,12 +1,10 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
+import List from "./ToDoList.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <App />
-
+  <StrictMode>
+    <List />
+  </StrictMode>
 );
-
